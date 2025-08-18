6e565a54e14fff30bd9ee464ae85cd3caa6738f8
Mohamed Mahmoud <133065999+MohamedElkhlawy153@users.noreply.github.com>
2025-08-18 23:46:22 +0300

    create prosses endpoint to chunk file

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignal
 import os
 import aiofiles
@@ -70,11 +70,51 @@ async def upload_data(project_id: str, file: UploadFile,
 
 
 @data_router.post("/process/{project_id}")
-async def process_data(project_id: str, request: ProcessRequest):
-    
-    file_id = request.file_id
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
 
-    return file_id
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.File_Processing_Failed.value
+
+            }
+        )
+
+    # Convert Document objects to serializable dictionaries
+    serializable_chunks = []
+    for chunk in file_chunks:
+        serializable_chunks.append({
+            "page_content": chunk.page_content,
+            "metadata": chunk.metadata
+        })
+    
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": "success",
+            "signal": ResponseSignal.File_Chunks_Generated.value,
+            "file_chunks": serializable_chunks,
+            "file_id": file_id
+        }
+    )
+
+
 
 
 
