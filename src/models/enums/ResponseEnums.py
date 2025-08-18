2dfd13bd2f8a9ab2857cf31fe4ff3863cc2e694d
Mohamed Mahmoud <133065999+MohamedElkhlawy153@users.noreply.github.com>
2025-08-18 04:30:40 +0300

    create enum directory and fResponseEnums file

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+    File_Validation_Passed = "File validation passed"
+    File_Type_Not_Allowed = "File type not allowed"
+    File_Size_Exceeds_Limit = "File size exceeds the limit"
+    File_uploaded_Successfully = "File uploaded successfully"
+    File_Failed = "File upload failed"
+
