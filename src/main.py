cf410373e6d00d90f6b6b8d2c2d3b83946f68ffc
Mohamed Mahmoud <133065999+MohamedElkhlawy153@users.noreply.github.com>
2025-08-19 21:43:00 +0300

    use motor to connect mongodb and database name

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    settings = get_settings()
+    app.mongodb_conn = AsyncIOMotorClient(settings.MONGODB_URI)
+    app.db_client = app.mongodb_conn[settings.MONGODB_DATABASE]
+    yield
+    
+    # shutdown
+    app.mongodb_conn.close()
+
+app = FastAPI(lifespan=lifespan)
 
 
-
-app = FastAPI()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
